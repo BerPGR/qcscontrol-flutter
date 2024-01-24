fc12c5d5196be0bbdfae9b5fdcf12c0d6de8dee1
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2024-01-24 10:39:01 -0300

    Added firebase options to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:qualitycontrol/screens/home_page.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
